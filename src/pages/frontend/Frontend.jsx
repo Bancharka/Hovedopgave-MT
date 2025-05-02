4e1f8c491731f48789e8a78a4fc8d6dabb0926de
Bancharka <olipet101@gmail.com>
2025-05-02 12:52:50 +0200

    Header struktureret og stylet
    Der mangler at blive lavet javascript til darkmode og lightmode, samt lavet navigering til de andre sider når de bliver lavet

---
+++ src/pages/frontend/Frontend.jsx
@@ -0,0 +1,13 @@
+import './FrontendStyle.scss'
+import Header from '../../components/Header/Header.jsx'
+
+export default function Frontend() {
+    return (
+        <>
+        <section className="frontend">
+            <Header />
+            
+        </section>
+        </>
+    )
+}
