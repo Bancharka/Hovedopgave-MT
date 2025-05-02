13c1d14618dd1d01ede4a43c0440eac1d11db135
Vivijensen <vivien.sorn@gmail.com>
2025-05-02 12:54:36 +0200

    last changes

--- src/pages/departments/Departments.jsx
+++ src/pages/departments/Departments.jsx
@@ -6,30 +6,41 @@ export default function Departments() {
       <img className="departments__wave departments__wave--left" src="src/assets/img/wave-left.svg" alt="wave left" />
       <img className="departments__wave departments__wave--right" src="src/assets/img/wave-right.svg" alt="wave right" />
 
-
       <div className="departments__content">
-        <p>VELKOMMEN TIL</p>
-        <img src="src/assets/img/logo-mt.svg" alt="morningtrain logo" />
+
+        <div className="departments__header">
+          <div className="departments__logo-wrapper">
+            <p className="departments__intro">VELKOMMEN TIL</p>
+            <img className="departments__logo" src="src/assets/img/logo-mt.svg" alt="morningtrain logo" />
+          </div>
+        </div>
+
 
         <div className="departments__pillars">
           <div className="pillar">
             <img src="src/assets/img/betina-pill.png" alt="Betina" />
             <a href="#">Marketing</a>
           </div>
+
           <div className="pillar">
             <img src="src/assets/img/jakob-pill.png" alt="Jakob" />
             <a href="/Frontend.jsx">Frontend</a>
           </div>
+
           <div className="pillar">
             <img src="src/assets/img/emelie-pill.png" alt="Emelie" />
             <a href="#">Design</a>
           </div>
+
           <div className="pillar">
             <img src="src/assets/img/mathias-pill.png" alt="Mathias" />
             <a href="#">Backend</a>
           </div>
+
         </div>
+
       </div>
+
     </section>
   );
 }
