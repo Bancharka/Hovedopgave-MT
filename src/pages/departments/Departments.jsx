61bae14b6ed93e6d7c861df714330b1c13b167a2
Vivijensen <vivien.sorn@gmail.com>
2025-05-02 09:25:48 +0200

    frontpage styling

--- src/pages/departments/Departments.jsx
+++ src/pages/departments/Departments.jsx
@@ -1,42 +1,35 @@
-import './Departmentsstyle.scss'
+import './Departmentsstyle.scss';
 
 export default function Departments() {
   return (
-    <>
-      <section className="departments">
+    <section className="departments">
+      <img className="departments__wave departments__wave--left" src="src/assets/img/wave-left.svg" alt="wave left" />
+      <img className="departments__wave departments__wave--right" src="src/assets/img/wave-right.svg" alt="wave right" />
 
-        <div>
-          <p className>VELKOMMEN TIL</p>
-          <img src="src\assets\img\logo-mt.svg" alt="morningtrain logo" />
-        </div>
 
-        <div>
+      <div className="departments__content">
+        <p>VELKOMMEN TIL</p>
+        <img src="src/assets/img/logo-mt.svg" alt="morningtrain logo" />
 
-          <div>
-            <img src="src\assets\img\betina-pill.png" alt="" />
-            <a href="">Marketing</a>
+        <div className="departments__pillars">
+          <div className="pillar">
+            <img src="src/assets/img/betina-pill.png" alt="Betina" />
+            <a href="#">Marketing</a>
           </div>
-
-          <div>
-            <img src="src\assets\img\jakob-pill.png" alt="" />
+          <div className="pillar">
+            <img src="src/assets/img/jakob-pill.png" alt="Jakob" />
             <a href="/Frontend.jsx">Frontend</a>
           </div>
-
-          <div>
-            <img src="src\assets\img\emelie-pill.png" alt="" />
-            <a href="">Design</a>
+          <div className="pillar">
+            <img src="src/assets/img/emelie-pill.png" alt="Emelie" />
+            <a href="#">Design</a>
           </div>
-
-
-          <div>
-            <img src="src\assets\img\mathias-pill.png" alt="" />
-            <a href="">Backend</a>
+          <div className="pillar">
+            <img src="src/assets/img/mathias-pill.png" alt="Mathias" />
+            <a href="#">Backend</a>
           </div>
-
-        <img className='departments__wave--left' src="src\assets\img\waves.svg" alt="wave graphic" />
-        <img className='departments__wave--right' src="src\assets\img\waves.svg" alt="" />
         </div>
-      </section>
-    </>
-  )
+      </div>
+    </section>
+  );
 }
