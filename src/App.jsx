2312532a2f208ea3393d71ebc19c4f43d7f9ef91
Clara la Cour <denlacour@gmail.com>
2025-05-02 11:38:00 +0200

    variables and header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Departments from './pages/departments/Departments'
+import Frontend from './pages/frontend/Frontend'
 
 export default function App() {
   return (
     <div>
       <nav>
         <Link to="/">Departments</Link>
+        <Link to="/Frontend">Frontend</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Departments />} />
+        <Route path="/Frontend" element={<Frontend />} />
       </Routes>
     </div>
   )
---
+++ src/pages/frontend/Frontend.jsx
@@ -0,0 +1,17 @@
+import './FrontendStyle.scss'
+import ContentHeader from '../../components/contentheader/ContentHeader'
+
+export default function Frontend() {
+    return (
+        <>
+        <section className="frontend">
+        <ContentHeader 
+            tagline="Velkommen til"
+            heading="Frontend Udvikling"
+            description="Her er alt hvad du skal vide om frontend udvikling hos Morning Train. Du kan finde information om vores frontend udviklingsafdeling, vores projekter og vores teammedlemmer. Vi håber, at du vil finde det nyttigt og inspirerende."
+        />
+        </section>
+        </>
+    )
+}
+
