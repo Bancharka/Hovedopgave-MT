9e662901c27174bf8df87dcbec1f982ff57eaaea
Clara la Cour <denlacour@gmail.com>
2025-05-07 13:11:06 +0200

    📱 work on responsive design

--- src/components/contentheader/ContentHeader.jsx
+++ src/components/contentheader/ContentHeader.jsx
@@ -5,7 +5,7 @@ export default function ContentHeader({ tagline, heading, description }) {
         <>
         <section className='contentheader'>
             <p className='contentheader__tagline tagline'>{tagline}</p>
-            <h1 className='contentheader__heading heading-1'>{heading}</h1>
+            <h1 className='contentheader__heading'>{heading}</h1>
             <p className='contentheader__description'>{description}</p>
         </section>
         </>
