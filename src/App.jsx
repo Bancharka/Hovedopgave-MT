958ed46b83c95b254da575fb34068fad4d605a9e
Bancharka <olipet101@gmail.com>
2025-05-13 10:30:02 +0200

    Oprette kultur siden

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Departments from './pages/departments/Departments'
 import Practical from './pages/practical/Practical'
+import Culture from "./pages/culture/Culture"
 
 export default function App() {
   return (
@@ -8,10 +9,12 @@ export default function App() {
       <nav>
         <Link to="/">Departments</Link>
         <Link to="/Practical">Praktisk info</Link>
+        <Link to="/Culture">Kultur</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Departments />} />
         <Route path="/Practical" element={<Practical />} />
+        <Route path="/Culture" element={<Culture />} />
       </Routes>
     </div>
   )
---
+++ src/pages/culture/Culture.jsx
@@ -0,0 +1,23 @@
+import Footer from "../../components/footer/Footer";
+import "./Culturestyle.scss";
+
+export default function Culture() {
+	return (
+		<>
+			<section className="culture">
+				<div>
+					<p className="tagline">SÅ ER DU KLAR!</p>
+					<h1>Kultur</h1>
+					<p>
+						Sund fornuft, fleksible arbejdsrammer som rummer livet,
+						autonome & ansvarlige arbejdsgange samt gode sociale
+						tilbud. Det er Morningtrains opskrift på det gode
+						arbejdsliv.
+					</p>
+				</div>
+			</section>
+
+			<Footer />
+		</>
+	);
+}
