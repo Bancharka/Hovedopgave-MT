0a70121fa4e26598f6f42ad3ac7726cd03b81c9f
Clara la Cour <denlacour@gmail.com>
2025-05-15 10:58:35 +0200

    testimonial done juhu

--- src/pages/frontpage/Frontpage.jsx
+++ src/pages/frontpage/Frontpage.jsx
@@ -10,7 +10,7 @@ export default function Departments() {
 				<div>
 					<p>VELKOMMEN TIL</p>
 					<img
-						src="public\img\onboardingtrain.svg"
+						src="/img/onboardingtrain.svg"
 						alt="onboradingtrain logo"
 					/>
 				</div>
@@ -20,7 +20,7 @@ export default function Departments() {
 				<div>
 					<img
 						className="frontpage__collage"
-						src="\img\slack_header.jpg"
+						src="/img/slack_header.jpg"
 						alt=""
 					/>
 					<h3 className="frontpage__description">
