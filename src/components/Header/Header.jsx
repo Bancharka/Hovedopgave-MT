e33ab43eecf02f094c8ac81ab539f0e2eaa74bb1
Bancharka <olipet101@gmail.com>
2025-05-15 14:03:27 +0200

    flytter billeder over i header mappe

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,41 @@
-import { Link } from 'react-router-dom'
+
+import { Link } from "react-router-dom";
 import "./HeaderStyle.scss";
 
 export default function Header() {
-	return (
-		<>
-			<section className="headerstyling">
-				<img
-					className="headerstyling__wave"
-					src="\img\wave-header.svg"
-					alt="Orange wave graphic"
-				/>
-				<div className="overnav">
-					<a href="/">
-						<img
-							className="overnav__morningtrain"
-							src="\img\logo-mt.svg"
-							alt="Morningtrain logo"
-						/>
-					</a>
-					<nav className="navigation">
-						<Link to="/Practical">Praktisk info</Link>
-						<Link to="/Culture">Kultur</Link>
-						<Link to="/Practical">Ressourcer</Link>
-						<Link to="/Practical">Teamet</Link>
-						<Link to="/Testimonials">Tidligere praktikanter</Link>
-					</nav>
-					<img
-						className="overnav__moon"
-						src="\img\icon _moon.svg"
-						alt="Darkmode icon"
-					/>
-				</div>
-			</section>
-		</>
-	);
-}
+ 	return (
+ 		<>
+ 			<section className="headerstyling">
+ 				<img
+ 					className="headerstyling__wave"
+ 					src="/img/header/wave-header.svg"
+ 					alt="Orange wave graphic"
+ 				/>
+ 				<div className="overnav">
+ 					<a href="/">
+ 						<img
+ 							className="overnav__morningtrain"
+ 							src="/img/header/logo-mt.svg"
+ 							alt="Morningtrain logo"
+ 						/>
+ 					</a>
+ 					<nav className="navigation">
+ 						<Link to="/Practical">Praktisk info</Link>
+ 						<Link to="/Culture">Kultur</Link>
+ 						<Link to="/Practical">Ressourcer</Link>
+ 						<Link to="/Practical">Teamet</Link>
+ 						<Link to="/Testimonials">Tidligere praktikanter</Link>
+ 					</nav>
+ 					<img
+ 						className="overnav__moon"
+ 						src="/img/header/icon _moon.svg"
+ 						alt="Darkmode icon"
+ 					/>
+ 				</div>
+ 			</section>
+ 		</>
+ 	);
+ }
+
+
+
