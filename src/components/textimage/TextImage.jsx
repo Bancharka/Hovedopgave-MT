72e485592c8d9f5e2c73f67489d780f22e793d6d
Clara la Cour <denlacour@gmail.com>
2025-05-16 09:37:52 +0200

    👏 merge complete

--- src/components/textimage/TextImage.jsx
+++ src/components/textimage/TextImage.jsx
@@ -1,13 +1,32 @@
-import './TextImageStyle.scss'
+import './TextImage.scss'
 
-export default function TextImage({ heading, description, image, imageFirst = false, altText }) {
-    return (
-        <section className={`textimage ${imageFirst ? 'textimage--reverse' : ''}`}>
-            <img className='textimage__image' src={image} alt={altText} />
-            <div className="textimage__content">
-                <h2 className='textimage__heading heading-1'>{heading}</h2>
-                <p className='textimage__description'>{description}</p>
-            </div>
-        </section>
-    );
+export default function TextImage({
+  category,
+  heading,
+  description,
+  contentsubheader,
+  paragraph1,
+  paragraph_bold1,
+  paragraph2,
+  paragraph_bold2,
+  image,
+  imageFirst = false,
+  altText = ''
+}) {
+  return (
+    <section className={`textimage ${imageFirst ? 'textimage--reverse' : ''}`}>
+      {image && <img className='textimage__image' src={image} alt={altText} />}
+      
+      <div className="textimage__content">
+        {category && <h3 className="textimage__category">{category}</h3>}
+        {heading && <h2 className="textimage__heading heading-1">{heading}</h2>}
+        {description && <p className="textimage__description">{description}</p>}
+        {contentsubheader && <h3 className="textimage__contentsubheader">{contentsubheader}</h3>}
+        {paragraph_bold1 && <p className="textimage__paragraph__bold1">{paragraph_bold1}</p>}
+        {paragraph1 && <p className="textimage__paragraph1">{paragraph1}</p>}
+        {paragraph_bold2 && <p className="textimage__paragraph__bold2">{paragraph_bold2}</p>}
+        {paragraph2 && <p className="textimage__paragraph2">{paragraph2}</p>}
+      </div>
+    </section>
+  );
 }
