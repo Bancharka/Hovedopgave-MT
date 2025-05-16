9065fcc416f32ccd1000f54de8eac2f00705d252
Clara la Cour <denlacour@gmail.com>
2025-05-16 10:40:02 +0200

    basically el finito

--- src/components/textimage/TextImage.jsx
+++ src/components/textimage/TextImage.jsx
@@ -1,32 +1,43 @@
 import './TextImage.scss'
 
 export default function TextImage({
-  category,
-  heading,
-  description,
-  contentsubheader,
-  paragraph1,
-  paragraph_bold1,
-  paragraph2,
-  paragraph_bold2,
-  image,
-  imageFirst = false,
-  altText = ''
+    category,
+    heading,
+    description,
+    listItems = [],
+    contentsubheader,
+    paragraph1,
+    paragraph_bold1,
+    paragraph2,
+    paragraph_bold2,
+    image,
+    imageFirst = false,
+    altText = ''
 }) {
-  return (
-    <section className={`textimage ${imageFirst ? 'textimage--reverse' : ''}`}>
-      {image && <img className='textimage__image' src={image} alt={altText} />}
-      
-      <div className="textimage__content">
-        {category && <h3 className="textimage__category">{category}</h3>}
-        {heading && <h2 className="textimage__heading heading-1">{heading}</h2>}
-        {description && <p className="textimage__description">{description}</p>}
-        {contentsubheader && <h3 className="textimage__contentsubheader">{contentsubheader}</h3>}
-        {paragraph_bold1 && <p className="textimage__paragraph__bold1">{paragraph_bold1}</p>}
-        {paragraph1 && <p className="textimage__paragraph1">{paragraph1}</p>}
-        {paragraph_bold2 && <p className="textimage__paragraph__bold2">{paragraph_bold2}</p>}
-        {paragraph2 && <p className="textimage__paragraph2">{paragraph2}</p>}
-      </div>
-    </section>
-  );
+    return (
+        <section className={`textimage ${imageFirst ? 'textimage--reverse' : ''}`}>
+            {image && <img className='textimage__image' src={image} alt={altText} />}
+
+            <div className="textimage__content">
+                {category && <h3 className="textimage__category">{category}</h3>}
+                {heading && <h2 className="textimage__heading heading-1">{heading}</h2>}
+                {description && <p className="textimage__description">{description}</p>}
+
+
+                {contentsubheader && <h3 className="textimage__contentsubheader">{contentsubheader}</h3>}
+                {paragraph_bold1 && <p className="textimage__paragraph__bold1">{paragraph_bold1}</p>}
+                {paragraph1 && <p className="textimage__paragraph1">{paragraph1}</p>}
+                {paragraph_bold2 && <p className="textimage__paragraph__bold2">{paragraph_bold2}</p>}
+
+                {listItems.length > 0 && (
+                    <ul className="textimage__list">
+                        {listItems.map((item, index) => (
+                            <li key={index} className="textimage__list-item">{item}</li>
+                        ))}
+                    </ul>
+                )}
+                {paragraph2 && <p className="textimage__paragraph2">{paragraph2}</p>}
+            </div>
+        </section>
+    );
 }
